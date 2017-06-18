7567de24a4940bdf049e81ea16c370b98f817dc7
Yasser Hennawi <yahennawi@gmail.com>
2017-06-18 21:15:47 +0200

    Initializing Repo

---
+++ src/main/java/com/mycompany/herokutest/main.java
@@ -0,0 +1,10 @@
+package com.mycompany.herokutest;
+import static spark.Spark.*;
+
+public class main {
+
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World");
+    }
+    
+}
